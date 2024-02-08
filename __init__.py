4189bb83d42441fb96a8d9efff62cd5dc221c6cb
amdou1 <156303191+amdou1@users.noreply.github.com>
2024-02-08 12:41:18 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,17 +6,5 @@ app = Flask(__name__) #creating flask app name
 def home():
     return render_template("index.html")
 
-@app.route('/resume_1')
-def resume_1():
-    return render_template("resume_1.html")
-
-@app.route('/resume_2')
-def resume_2():
-    return render_template("resume_2.html")
-
-@app.route('/resume_template')
-def resume_template():
-    return render_template("resume_template.html")
-
 if(__name__ == "__main__"):
     app.run()
